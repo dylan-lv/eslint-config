b4f0c7ba979bb5cc3baa8dcff9798beca297405d
lvhaizhou <417703682@qq.com>
2022-03-14 10:52:36 +0800

    base

--- packages/basic/index.js
+++ packages/basic/index.js
@@ -1,3 +1,17 @@
 module.exports = {
-
+  env: {
+    es6: true,
+    browser: true,
+    node: true
+  },
+  extends: [
+    "standard"
+  ],
+  ignorePatterns: [
+    "*.min.*"
+  ],
+  rules: {
+    // common
+    quotes: ["error", "double"]
+  }
 }
